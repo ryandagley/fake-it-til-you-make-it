76cea8917cbb447fc4d93d1b043d1b9569bb252a
ohhidenny <rhdagley@gmail.com>
2018-10-03 17:39:43 -0700

    Adds connector to sqlite database.

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Dumping stuff as I learn.
 - random_number_comparison:  I wrote this script when 
 thinking of an interview question I bombed.  
 I solved it and then some!
+
+- sqlite_connect: I wanted to make a database for some
+sensors.
--- sqlite_connect.py
+++ sqlite_connect.py
@@ -4,11 +4,25 @@ Database for tracking some sensors.
 
 import sqlite3
 connection = sqlite3.connect('my_project.db')
-cursor = connection.cursor()
+c = connection.cursor()
 
-cursor.execute('CREATE TABLE sensors (sensor_id integer PRIMARY KEY, sensor_type text NOT NULL, \
-sensor_name text NOT NULL UNIQUE, installation_date datetime)')
+#Table creation
+#c.execute('CREATE TABLE sensors (sensor_id integer PRIMARY KEY, sensor_type text NOT NULL, \
+#sensor_name text NOT NULL UNIQUE, installation_date datetime)')
 
-#cursor.execute('DELETE FROM sensors')
-#cursor.execute('DROP TABLE sensors')
+#c.execute('DELETE FROM sensors')
+#c.execute('DROP TABLE sensors')
+
+#INSERTs
+#c.execute('\
+#INSERT INTO sensors\
+#(sensor_id, sensor_type, sensor_name, installation_date)\
+#VALUES (2, "soil", "Charlie", 10-3-2018)')
+
+#Printing SELECT query
+c.execute('SELECT * FROM sensors')
+all_rows = c.fetchall()
+print('1):', all_rows)
+
 connection.commit()
+
