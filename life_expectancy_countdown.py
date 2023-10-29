c41e49b174f87d4f0022589354d115488c109ce0
unknown <rhdagley@gmail.com>
2023-10-28 17:07:27 -0700

    Cleans up code to where time remaining is accurate descriptor

--- life_expectancy_countdown.py
+++ life_expectancy_countdown.py
@@ -15,21 +15,6 @@ current_datetime = datetime.datetime.now()
 target_datetime = datetime.datetime(target_year, birthdate.month, birthdate.day, 0, 0, 0)
 time_remaining = target_datetime - current_datetime
 
-# Calculate years, months, days, hours, minutes, and seconds
-days = time_remaining.days
-seconds = time_remaining.seconds
-years = days // 365
-days %= 365
-months = days // 30
-days %= 30
-hours, remainder = divmod(seconds, 3600)
-minutes, seconds = divmod(remainder, 60)
-
-# Calculate the number of weeks remaining
-weeks_remaining = days // 7
-
-# Calculate the total number of weeks from today until your 76th birthday
-total_weeks = (target_datetime - current_datetime).days // 7
 
 while True:
     # Calculate the current time
@@ -39,28 +24,28 @@ while True:
     time_remaining = target_datetime - current_datetime
 
     # Calculate years, months, days, hours, minutes, and seconds
-    days = time_remaining.days
-    seconds = time_remaining.seconds
-    years = days // 365
-    days %= 365
-    months = days // 30
-    days %= 30
-    hours, remainder = divmod(seconds, 3600)
-    minutes, seconds = divmod(remainder, 60)
+    days_remaining = time_remaining.days
+    seconds_remaining = time_remaining.seconds
+    years_remaining = days_remaining // 365
+    days_remaining %= 365
+    months_remaining = days_remaining // 30
+    days_remaining %= 30
+    hours_remaining, remainder = divmod(seconds_remaining, 3600)
+    minutes_remaining, seconds_remaining = divmod(remainder, 60)
 
     # Calculate the number of weeks remaining
-    weeks_remaining = days // 7
+    weeks_remaining = days_remaining // 7
 
     # Calculate the total number of weeks from today until your 76th birthday
     total_weeks = (target_datetime - current_datetime).days // 7
 
     # Calculate periods for the years you've lived and have left
-    years_lived = years
-    years_left = 76 - years_lived
+    years_lived = 76 - years_remaining
 
     # Clear the previous line and print the countdown and total weeks
     os.system('cls' if os.name == 'nt' else 'clear')  # Clear the screen
-    print(f" {'.' * years_left} {years_left} years lived \n {'.' * years_lived} {years_lived} years left \n" , end="")
-    print(f" | Countdown: {years} years, {months} months, {days} days, {hours} hours, {minutes} minutes, {seconds} seconds",  end="" )
-    print(f" ({total_weeks} weeks in total)", end="\r")  # Print on the same line
+    print(f" Final days:  {target_datetime}") #final days
+    print(f" {'.' * years_lived} {years_lived} years lived \n {'.' * years_remaining} {years_remaining} years left \n" , end="")
+    print(f" | Countdown: {years_remaining} years, {months_remaining} months, {days_remaining} days, {hours_remaining} hours, {minutes_remaining} minutes, {seconds_remaining} seconds",  end="" )
+    print(f" ({total_weeks} weeks left)", end="\r")  # Print on the same line
     time.sleep(1)
