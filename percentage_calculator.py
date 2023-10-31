201c2048a634e0c2001ea2b717c5946b1fc5d353
Ryan Dagley <rhdagley@gmail.com>
2023-10-31 15:39:29 -0700

    Update percentage_calculator.py with additional functionality

--- percentage_calculator.py
+++ percentage_calculator.py
@@ -1,6 +1,26 @@
-print("This will tell you X is what percent of Y.")
+def what_is_X_percent_of_Y():
+    print("This will tell you what is X percent of Y.")
 
-total = int(input("What is the total? y = \n"))
-progress = int(input("How much have you completed? x = \n"))
+    total = int(input("What is the total? y = \n"))
+    progress = int(input("How much have you completed? x = \n"))
 
-print(str(progress) + " is " + str(progress/total*100) + " percent of " + str(total))
+    print(str(progress/total*100) + " percent of " + str(total) + " is " +  (str(progress)))
+
+def X_is_what_percent_of_Y():
+    print("This will tell you what percent X is of Y.")
+    total = int(input("What is the total? y = \n"))
+    progress = int(input("What is X? x = \n"))
+
+    print(str(progress) + " is " + str(progress/total*100) + " percent of " + str(total))
+
+def main():
+    choice = input("What calculator?  1.) What is X% of Y? 2.) What % of X is Y? ") 
+
+    if choice == '1':
+        what_is_X_percent_of_Y()
+    elif choice == '2':
+        X_is_what_percent_of_Y()
+    else:
+        print("Invalid choice.  Try again.")
+
+main()
