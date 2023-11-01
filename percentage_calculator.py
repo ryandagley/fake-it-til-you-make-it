adb440132fd34a847a16b3840e698a9bbd5c95b8
Ryan Dagley <rhdagley@gmail.com>
2023-11-01 15:07:28 -0700

    Added percent increase or decrease calculation to percentage calculator

--- percentage_calculator.py
+++ percentage_calculator.py
@@ -13,13 +13,28 @@ def X_is_what_percent_of_Y():
 
     print(str(progress) + " is " + str(progress/total*100) + " percent of " + str(total))
 
+def percentage_increase_drecrease():
+    print("This will tell you the percentage increase or decrease from X to Y.")
+    start_value = int(input("What is the starting percentage?  x = \n"))
+    end_value = int(input("What is the second value? y = \n"))
+    value_diff = int(start_value-end_value)
+    if value_diff < 0:
+        direction = "increase"
+    elif value_diff > 0:
+        direction = "decrease"
+    else: 
+        direction = "changed"
+    print(str(abs((value_diff)/start_value) * 100) + "% " + direction)
+
 def main():
-    choice = input("What calculator?  1.) What is X% of Y? 2.) What % of X is Y? ") 
+    choice = input("What calculator?  1.) What is X% of Y? 2.) What % of X is Y? 3.) What is the % increase or decrease?") 
 
     if choice == '1':
         what_is_X_percent_of_Y()
     elif choice == '2':
         X_is_what_percent_of_Y()
+    elif choice == '3':
+        percentage_increase_drecrease()
     else:
         print("Invalid choice.  Try again.")
 
