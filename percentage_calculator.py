167447801b9650e5622a194fd411be95f6bc0c51
Ryan Dagley <rhdagley@gmail.com>
2023-11-02 10:13:52 -0700

    Update percentage_calculator.py with increase decrease functions.

--- percentage_calculator.py
+++ percentage_calculator.py
@@ -26,8 +26,31 @@ def percentage_increase_drecrease():
         direction = "changed"
     print(str(abs((value_diff)/start_value) * 100) + "% " + direction)
 
+def add_percentage_to_X():
+    print("This will add Y% to X.")
+    start_value = int(input("What is the starting value? x = \n"))
+    percentage_increase = int(input("What percentage are you adding to " + str(start_value) + "? "))
+    percentage_increase = (percentage_increase/100)
+    print(str((start_value * percentage_increase) + start_value))
+
+def subtract_percentage_from_X():
+    print("This will subtract Y% from X.")
+    start_value = int(input("What is the starting value? x = \n"))
+    percentage_decrease = int(input("What percentage are you subtracting from " + str(start_value) + "? "))
+    percentage_decrease = (percentage_decrease/100)
+    print(str(start_value - (start_value * percentage_decrease)))
+
 def main():
-    choice = input("What calculator?  1.) What is X% of Y? 2.) What % of X is Y? 3.) What is the % increase or decrease?") 
+    choice = input("""
+    Which calculator? \n 
+    1.) What is X% of Y? \n 
+    2.) What % of X is Y? \n 
+    3.) What is the % increase or decrease? \n
+    4.) What is X + Y%? \n
+    5.) What is X - Y%? \n
+                    
+                    
+    Choice: """) 
 
     if choice == '1':
         what_is_X_percent_of_Y()
@@ -35,7 +58,13 @@ def main():
         X_is_what_percent_of_Y()
     elif choice == '3':
         percentage_increase_drecrease()
+    elif choice == '4':
+        add_percentage_to_X()
+    elif choice == '5':
+        subtract_percentage_from_X()
     else:
         print("Invalid choice.  Try again.")
 
 main()
+
+
