f0c409c45d3e957b8b2edcae4a7709bc6036345c
unknown <rhdagley@gmail.com>
2023-11-03 19:07:45 -0700

    Refactor code to use OOP for percentage calculation

--- percentage_calculator.py
+++ percentage_calculator.py
@@ -4,14 +4,18 @@ def what_is_X_percent_of_Y():
     total = int(input("What is the total? y = \n"))
     progress = int(input("How much have you completed? x = \n"))
 
-    print(str(progress/total*100) + " percent of " + str(total) + " is " +  (str(progress)))
+    print(str(progress/total*100) + " percent of " +
+          str(total) + " is " + (str(progress)))
+
 
 def X_is_what_percent_of_Y():
     print("This will tell you what percent X is of Y.")
     total = int(input("What is the total? y = \n"))
     progress = int(input("What is X? x = \n"))
 
-    print(str(progress) + " is " + str(progress/total*100) + " percent of " + str(total))
+    print(str(progress) + " is " + str(progress /
+          total*100) + " percent of " + str(total))
+
 
 def percentage_increase_drecrease():
     print("This will tell you the percentage increase or decrease from X to Y.")
@@ -22,30 +26,37 @@ def percentage_increase_drecrease():
         direction = "increase"
     elif value_diff > 0:
         direction = "decrease"
-    else: 
+    else:
         direction = "changed"
     print(str(abs((value_diff)/start_value) * 100) + "% " + direction)
+
 
 def add_percentage_to_X():
     print("This will add Y% to X.")
     start_value = int(input("What is the starting value? x = \n"))
-    percentage_increase = int(input("What percentage are you adding to " + str(start_value) + "? "))
+    percentage_increase = int(
+        input("What percentage are you adding to " + str(start_value) + "? "))
     percentage_increase = (percentage_increase/100)
     print(str((start_value * percentage_increase) + start_value))
+
 
 def subtract_percentage_from_X():
     print("This will subtract Y% from X.")
     start_value = int(input("What is the starting value? x = \n"))
-    percentage_decrease = int(input("What percentage are you subtracting from " + str(start_value) + "? "))
+    percentage_decrease = int(
+        input("What percentage are you subtracting from " + str(start_value) + "? "))
     percentage_decrease = (percentage_decrease/100)
     print(str(start_value - (start_value * percentage_decrease)))
+
 
 def pareto():
     print("This will calculate the 80/20% split of X.")
     start_value = int(input("What is the starting value? x = \n"))
-    eighty = round(start_value * .8,2)
-    twenty = round(start_value * .2,2)
-    print(str(eighty) + "/" + str(twenty) + ".  80% of " + str(start_value) + " is " + str(eighty) + ".  20% of " + str(start_value) + " is " + str(twenty) + ".")
+    eighty = round(start_value * .8, 2)
+    twenty = round(start_value * .2, 2)
+    print(str(eighty) + "/" + str(twenty) + ".  80% of " + str(start_value) + " is " +
+          str(eighty) + ".  20% of " + str(start_value) + " is " + str(twenty) + ".")
+
 
 def main():
     choice = input("""
@@ -58,7 +69,7 @@ def main():
     6.) What is the 80/20 of X? \n
                     
                     
-    Choice: """) 
+    Choice: """)
 
     if choice == '1':
         what_is_X_percent_of_Y()
@@ -75,7 +86,5 @@ def main():
     else:
         print("Invalid choice.  Try again.")
 
+
 main()
-
-
-
