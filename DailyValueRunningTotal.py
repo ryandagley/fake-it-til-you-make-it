6c10c16af44312f032ec3d695f2349a8834194a7
Ryan Dagley <rhdagley@gmail.com>
2024-09-10 19:27:51 -0700

    Adds couples total

--- DailyValueRunningTotal.py
+++ DailyValueRunningTotal.py
@@ -15,4 +15,5 @@ running_total = 0
 # Loop through days 1 to 31
 for day in range(1, 32):
     running_total += day  # Add the current day to the running total
-    print(f"Day {day}: Value = {day}, Running Total = {running_total}")
+    print(f"Day {day}: Value = {day}, Running Total = {running_total}, Couple Running Total = {running_total*2}"
+        )
